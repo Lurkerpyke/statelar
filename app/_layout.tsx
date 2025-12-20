c4fc9f9154b92a88d051ddbece448bf6dc4f3857
Leandro <leandro.soares.eneterio@gmail.com>
2025-12-20 10:19:27 -0300

    v0.0.2 tested nativewind

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,6 +1,18 @@
-import { Stack } from "expo-router";
+// app/_layout.tsx
+import { Tabs } from "expo-router";
+import { SafeAreaProvider } from "react-native-safe-area-context";
 import "../global.css";
 
 export default function RootLayout() {
-  return <Stack />;
+  return (
+    <SafeAreaProvider>
+      <Tabs
+        screenOptions={{
+          tabBarStyle: {
+            position: 'absolute',
+          },
+        }}
+      />
+    </SafeAreaProvider>
+  );
 }
--- app/index.tsx
+++ app/index.tsx
@@ -1,10 +1,10 @@
-import { Text, View } from "react-native";
+import { Text } from "react-native";
+import { SafeAreaView } from 'react-native-safe-area-context';
 
 export default function Index() {
   return (
-    <View className="flex-1 items-center justify-center bg-blue-500"
-    >
+    <SafeAreaView className="flex-1 items-center justify-center bg-blue-500">
       <Text>Hello, World!</Text>
-    </View>
+    </SafeAreaView>
   );
 }
