6b8fb9322cd391403810beff82c6a9b8fd951f6a
Leandro <leandro.soares.eneterio@gmail.com>
2025-12-21 11:26:37 -0300

    v0.1.0 Sign up page

--- app/(root)/(tabs)/index.tsx
+++ app/(root)/(tabs)/index.tsx
@@ -4,18 +4,18 @@ import { SafeAreaView } from 'react-native-safe-area-context';
 
 export default function Index() {
   return (
-    <SafeAreaView className="flex-1 items-center justify-center bg-blue-500">
+    <SafeAreaView className="flex-1 items-center justify-center">
       <Link href="/explore">
-        <Text className="text-white text-lg">Go to Explore</Text>
+        <Text className=" text-4xl font-rubik-extraBold">Go to Explore</Text>
       </Link>
       <Link href="/profile">
-        <Text className="text-white text-lg">Go to Profile</Text>
+        <Text className=" text-lg">Go to Profile</Text>
       </Link>
       <Link href="/sign-in">
-        <Text className="text-white text-lg">Go to Sign In</Text>
+        <Text className=" text-lg">Go to Sign In</Text>
       </Link>
-      <Link href="/properties/1">
-        <Text className="text-white text-lg">Go to Properties</Text>
+      <Link href="/properties/01">
+        <Text className=" text-lg">Go to Properties</Text>
       </Link>
     </SafeAreaView>
   );
--- app/(root)/(tabs)/sign-in.tsx
+++ app/(root)/(tabs)/sign-in.tsx
@@ -1,11 +1,37 @@
-import { View, Text } from 'react-native';
+import { View, Text, ScrollView, Image, TouchableOpacity } from 'react-native';
 import React from 'react';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import images from '@/constants/images';
+import icons from '@/constants/icons';
 
 const SignIn = () => {
+
+  const handleLogin = () => {};
+
   return (
-    <View>
-      <Text>SignIn</Text>
-    </View>
+    <SafeAreaView className='bg-white h-full'>
+      <ScrollView contentContainerClassName='h-full'>
+        <Image
+          source={images.onboarding}
+          className="w-full h-4/6" resizeMode="contain">
+        </Image>
+
+        <View className='px-10'>
+          <Text className='font-rubik text-base uppercase text-center text-black-200'>Bem Vindo à Statelar</Text>
+          <Text className='font-rubik-bold text-3xl text-center text-black-300 mt-2'>Encontre a sua {"\n"}
+            <Text className='text-primary-300'>Casa Dos Sonhos</Text>
+          </Text>
+          <Text className='text-lg font-rubik text-black-200 text-center mt-12'>Faça login com Google</Text>
+          <TouchableOpacity onPress={handleLogin} className='bg-white shadow-lg shadow-zinc-500 rounded-full w-full py-4 mt-5'>
+            <View className='flex flex-row items-center justify-center'>
+              <Image source={icons.google} className='w-6 h-6' resizeMode='contain' />
+              <Text className='text-md font-rubik-medium text-black-300 ml-2'>Entrar com Google</Text>
+            </View>
+          </TouchableOpacity>
+        </View>
+
+      </ScrollView>
+    </SafeAreaView>
   )
 };
 
